d256ec9e17a80d99ee3678357b83490e8c7e77fe
Mark Du <dulongjiebackup@gmail.com>
2025-03-29 20:22:36 -0700

    add readline

--- chat_app/client.js
+++ chat_app/client.js
@@ -1,11 +1,36 @@
 const net = require('net')
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 
 const socket = net.createConnection({
     host:'127.0.0.1',
-    port:2022,
-}, () => {
-    const buff = Buffer.alloc(2);
-    buff[0] = 11;
-    buff[1] = 22;
-    socket.write(buff);
+    port:2025,
+},  () => {
+    console.log(`connect to server...`);
+    
+    rl.question("Input >>> ", (message) => {
+       socket.write(message);
+    });
+    
 })
+
+// Handle server response
+socket.on('data', (data) => {
+    console.log("Server:", data.toString());
+});
+
+// Handle connection close
+socket.on('end', () => {
+    console.log("Connection is ended...");
+    rl.close();
+});
+
+// Handle errors
+socket.on('error', (err) => {
+    console.error("Error:", err.message);
+    rl.close();
+});
--- chat_app/server.js
+++ chat_app/server.js
@@ -1,15 +1,29 @@
 const net = require('net')
 const http = require('http')
 
-const server = net.createServer((socket) => {
+const server = net.createServer();
+
+server.on('connection',(socket) => {
+    console.log(`New connection to server...`);
+    
     socket.on('data', (data) => {
-        console.log(data);
+        console.log(data.toString('utf-8'));
         
     })
-
+    
 })
 
-server.listen(2022,'127.0.0.1', () => {
+server.on('error', (err) => {
+    console.log("wrong");
+    
+})
+
+server.on('close', () => {
+    console.log("close");
+    
+})
+
+server.listen(2025,'127.0.0.1', () => {
     console.log(`Server is running... on`, server.address());
     
 } )
