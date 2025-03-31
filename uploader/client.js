404b11ff2daf0262bd9c3b0f840b7b2ae2823996
Mark Du <dulongjiebackup@gmail.com>
2025-03-31 16:00:24 -0700

    copy filename

--- uploader/client.js
+++ uploader/client.js
@@ -1,17 +1,52 @@
 const net = require('net')
 const fs = require('node:fs/promises')
+const path = require('path')
+
+const clearLine = (dir) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.clearLine(dir, () => resolve());
+    });
+};
+
+const moveCursor = (dx, dy) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.moveCursor(dx,dy, () => resolve());
+    });
+};
 
 const socket = net.createConnection({
     host:'::1',
     port:5050,
 }, async () => {
-    const filePath = './uploader/text.txt';
+    const filePath = process.argv[2];
+    const fileName =  path.basename(filePath);
     const fileHandle = await fs.open(filePath, 'r');
     const fileStream = fileHandle.createReadStream();
+
+    //calculate upload percentage
+    const fileSize = (await fileHandle.stat()).size;
+    let uploadPercentage = 0;
+    let bytesUploaded = 0;
+
+    socket.write(`FileName: ${fileName}------`);
+
+
+console.log();
     
-    fileStream.on('data', (data) => {
+    fileStream.on('data', async (data) => {
         if(!socket.write(data)){
             fileStream.pause();
+        }
+
+        bytesUploaded += data.length;
+        let updatedPercentage = Math.floor((bytesUploaded/fileSize) * 100);
+
+        if(updatedPercentage !== uploadPercentage){
+            uploadPercentage = updatedPercentage;
+            await moveCursor(0, -1);
+            await clearLine(0);
+            console.log(`uploading...${uploadPercentage}%`);
+            
         }
     });
 
--- uploader/server.js
+++ uploader/server.js
@@ -4,19 +4,26 @@ const fs = require('node:fs/promises')
 
 const server = net.createServer()
 
-let fileHandle, fileStream;
 
 server.on('connection', (socket) => {
     console.log("New connection");
-
+    
+    let fileHandle, fileStream;
     socket.on('data', async (data) => {
         if(!fileHandle){
             socket.pause();
-            fileHandle = await fs.open(`./uploader/storage/test.txt`, 'w');
+
+            const indexOfDiv = data.indexOf('------');
+            console.log(indexOfDiv);
+            
+            const fileName = data.subarray(10, indexOfDiv).toString();
+            console.log(fileName);
+            fileHandle = await fs.open(`./uploader/storage/${fileName}`, 'w');
    
             fileStream = fileHandle.createWriteStream();
             fileHandle.write(data);
 
+            fileStream.write(data.subarray(indexOfDiv + 6));
             socket.resume();
             fileStream.on('drain', () => {
                 socket.resume();
