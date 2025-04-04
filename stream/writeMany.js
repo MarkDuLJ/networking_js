793f2e78273f461373c4476e81cf87c0aee1dee0
Mark Du <dulongjiebackup@gmail.com>
2025-04-04 11:03:34 -0700

    drain then write

--- stream/writeMany.js
+++ stream/writeMany.js
@@ -53,20 +53,58 @@ fs.open('./stream/text.txt', 'w', (err,fd) => {
  * Use stream
  * time: 170ms
  * memory:200mB hard to see from active monitor because of frequency
- */
 
 const fs = require('node:fs/promises');
 
 (async () => {
     console.time("Stream_write_large_file");
-
+    
     const fileHandle = await fs.open('./stream/text.txt', 'w');
     const stream = fileHandle.createWriteStream();
-
+    
     for (let i = 0; i < 1000000; i++) {
         const buff = Buffer.from(`${i}\n`, 'utf-8');
         stream.write(buff);
     }
-
+    
     console.timeEnd("Stream_write_large_file");
 })();
+*/
+
+/**
+ * Use read/write stream to smooth memory usage
+ * time:
+ * memory:
+ */
+
+const fs = require('node:fs/promises');
+
+(
+    async () => {
+        console.time("R/W stream");
+
+        const fileHandle = await fs.open('./stream/text.txt', 'w');
+        const writeStream = fileHandle.createWriteStream();
+
+        let i = 0;
+
+        const writeBuff = () => {
+            for ( ;i < 1000000; i++) {
+                const buff = Buffer.from(`${i}`, 'utf-8');
+               if(!writeStream.write(buff)){
+                   break; 
+               }    
+            }
+        }
+
+        writeBuff();
+
+        writeStream.on('drain', ()=>{
+            console.log('cleaning buff...');
+            writeBuff();
+            
+        });
+
+        console.timeEnd("R/W stream");
+    }
+)();
